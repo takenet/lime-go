cf366d5d1fec6d80840d4c95cf5800c33048262a
André Bires <andrebires@gmail.com>
2020-04-24 19:10:21 -0300

    Changed enum validation strategy

--- session.go
+++ session.go
@@ -1,47 +1,48 @@
 package main
 
+import (
+	"fmt"
+)
+
 // Defines the supported session states
-type SessionState int
+type SessionState string
 
 const (
 	// The session is new and doesn't exists an established context.
 	// It is sent by a client node to start a session with a server.
-	SessionNew = iota
-	SessionNegotiating
-	SessionAuthenticating
-	SessionEstablished
-	SessionFinishing
-	SessionFinished
-	SessionFailed
+	SessionNew            = "new"
+	SessionNegotiating    = "negotiating"
+	SessionAuthenticating = "authenticating"
+	SessionEstablished    = "established"
+	SessionFinishing      = "finishing"
+	SessionFinished       = "finished"
+	SessionFailed         = "failed"
 )
 
-var sessionStateToString = map[SessionState]string{
-	SessionNew:            "new",
-	SessionNegotiating:    "negotiating",
-	SessionAuthenticating: "authenticating",
-	SessionEstablished:    "established",
-	SessionFinishing:      "finishing",
-	SessionFinished:       "finished",
-	SessionFailed:         "failed",
-}
+func (s SessionState) Validate() error {
+	switch s {
+	case SessionNew, SessionNegotiating, SessionAuthenticating, SessionEstablished, SessionFinishing, SessionFinished, SessionFailed:
+		return nil
+	}
 
-var sessionStateToID = map[string]SessionState{
-	"new":            SessionNew,
-	"negotiating":    SessionNegotiating,
-	"authenticating": SessionAuthenticating,
-	"established":    SessionEstablished,
-	"finishing":      SessionFinishing,
-	"finished":       SessionFinished,
-	"failed":         SessionFailed,
+	return fmt.Errorf("invalid session state '%v'", s)
 }
 
 func (s SessionState) MarshalText() ([]byte, error) {
-	return []byte(sessionStateToString[s]), nil
+	err := s.Validate()
+	if err != nil {
+		return []byte{}, err
+	}
+	return []byte(s), nil
 }
 
 func (s *SessionState) UnmarshalText(text []byte) error {
-	state := string(text)
-	*s = sessionStateToID[state]
+	state := SessionState(text)
+	err := state.Validate()
+	if err != nil {
+		return err
+	}
+	*s = state
 	return nil
 }
 
